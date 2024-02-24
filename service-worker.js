bba0b2956d1ba42f71647916f0b31f88828086db
Nexxuss (SniperLyfe) <nexxuss.exploits@gmail.com>
2024-02-24 12:02:30 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
+self.addEventListener('beforeinstallprompt', (event) => {
+       // Prevent Chrome 67 and earlier from automatically showing the prompt
+       event.preventDefault();
+       // Stash the event so it can be triggered later.
+       deferredPrompt = event;
+       // Update UI to notify the user they can add to home screen
+       showInstallPromotion();
+     });
 
+     function showInstallPromotion() {
+       // Your logic to display the install prompt to the user
+     }
+     
